d926baccbd31100376595a70bb4a3f069995e76c
subhashishshah <subhashish@ismt.edu.np>
2023-06-16 09:05:28 +0545

    For class

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const handObj = {
+  rock: "./assets/Rock.png",
+  paper: "./assets/Paper.png",
+  scissors: "./assets/Scissors.png",
+};
+// human le k click k bhayo bhanera patta lagam & store
+function human_choice(hands) {
+  //   alert(hands);
+  //   console.log(hands);
+  // choose gareko kura display huna parcha
+  let human_choice_image = document.getElementById("human_choice");
+  //   console.log(human_choice_image);
+  human_choice_image.src = "";
+}
+
+// computer ko choice patta lagaune
+function computer_choice() {}
+
+// k bhayo result patta lagaune
+function refree() {}
